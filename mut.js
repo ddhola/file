2087f4c52d1f998945a9b10dc5137dd10f1bb73c
ddhola <landwhite9@gmail.com>
2025-05-30 15:47:11 +0800

    Update mut.js

--- mut.js
+++ mut.js
@@ -7,6 +7,7 @@
 {"name":"AnboxII 🇹🇼 🇨🇳","url":"http://firebase.anbox118.workers.dev/","ua": "okhttp/3.15","epg": "http://iptv.52sw.top:668/?ch={name}&date={date}","logo":"https://epg.112114.xyz/logo/{name}.png"},  
 
 {"name":"1027🇹🇼","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/1027"},
+{"name":"0609🇹🇼","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/0609test"},
   
 
 
