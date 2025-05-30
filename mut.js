2b5a24857a07df70efa4ed36f3a7f18732c5da2d
ddhola <landwhite9@gmail.com>
2025-05-30 15:44:22 +0800

    Update mut.js

--- mut.js
+++ mut.js
@@ -6,6 +6,7 @@
 {"name": "立視 LiTV 🇹🇼(備份2)","type": 0,"url": "http://xg.52sw.top/iptv/litv.php","ua": "okhttp/3.15","epg": "http://iptv.52sw.top:668/?ch={name}&date={date}","logo":"https://epg.112114.xyz/logo/{name}.png"},
 {"name":"AnboxII 🇹🇼 🇨🇳","url":"http://firebase.anbox118.workers.dev/","ua": "okhttp/3.15","epg": "http://iptv.52sw.top:668/?ch={name}&date={date}","logo":"https://epg.112114.xyz/logo/{name}.png"},  
 
+{"name":"1027🇹🇼","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/1027"},
   
 
 
