5d4686e8e0768510ff3034640f1bc0d67b6648c3
ddhola <landwhite9@gmail.com>
2025-06-18 17:08:52 +0800

    Update a01test.js

--- ba/a01test.js
+++ ba/a01test.js
@@ -5,7 +5,7 @@
 "lives":[
 {"name":"59高解析🇹🇼","api":"csp_MQiTV","ext":"https://59.125.210.231:4433","jar":"./jar/fmMQiTV.jar","epg": "http://iptv.52sw.top:668/?ch={name}&date={date}","logo":"https://epg.112114.xyz/logo/{name}.png"},
 
-    {"name":"0609","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/0609test"}, 
+    {"name":"06091","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/0609test"}, 
     {"name":"1027","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/1027"}, 
       {
       "name": "BeeSport直播(python)",
