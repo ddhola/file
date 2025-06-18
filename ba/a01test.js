3813d56ac5fa54365aa716036d43c8789701b079
ddhola <landwhite9@gmail.com>
2025-06-18 16:58:15 +0800

    Create a01test.js

---
+++ ba/a01test.js
@@ -0,0 +1,32 @@
+
+
+ 
+
+   {
+  "lives": [
+    
+    {"name":"0609","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/0609test"}, 
+    {"name":"1027","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/1027"}, 
+      {
+      "name": "BeeSport直播(python)",
+      "type": 3,
+      "api": "./live_beesport.py",
+      "ext": {},
+      "header": {
+        "Referer": "https://p.m82xg4z0cdbz7.com/"
+      }
+    },
+    {
+      "name": "LITV直播(python)",
+      "type": 3,
+      "api": "./live_litv.py",
+      "ext": {}
+    },
+    {
+      "name": "SMT直播(python)",
+      "type": 3,
+      "api": "./live_smt.py",
+      "ext": {}
+    }
+  ]
+}
