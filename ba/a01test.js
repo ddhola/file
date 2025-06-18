c8691c24a24c39f26de5df6b1fe690bc733042c1
ddhola <landwhite9@gmail.com>
2025-06-18 17:04:05 +0800

    Update a01test.js

--- ba/a01test.js
+++ ba/a01test.js
@@ -1,10 +1,10 @@
 
+{
+"spider":"./jar/f0223.jar",
+"logo": "https://raw.githubusercontent.com/sqspot/tac/main/謝分享.gif",
+"lives":[
+{"name":"59高解析🇹🇼","api":"csp_MQiTV","ext":"https://59.125.210.231:4433","jar":"./jar/fmMQiTV.jar","epg": "http://iptv.52sw.top:668/?ch={name}&date={date}","logo":"https://epg.112114.xyz/logo/{name}.png"},
 
- 
-
-   {
-  "lives": [
-    
     {"name":"0609","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/0609test"}, 
     {"name":"1027","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/1027"}, 
       {
