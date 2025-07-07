81ebded9acee43c7c9663e731ccdf4c1e157763f
ddhola <landwhite9@gmail.com>
2025-07-07 19:30:29 +0800

    Create mut_gpt.js

---
+++ mut_gpt.js
@@ -0,0 +1,45 @@
+{
+  "spider": "https://raw.githubusercontent.com/ffmking/tv1/refs/heads/main/mtv.jar",
+  "lives": [
+    {
+      "name": "59",
+      "api": "csp_MQiTV",
+      "ext": "https://59.125.210.231:4433"
+    },
+    {
+      "name": "Ofiii線路",
+      "api": "csp_LiveQ",
+      "ext": "https://www.ofiii.com/api.php/app/live"
+    },
+    {
+      "name": "超級電視",
+      "api": "csp_LiveQ",
+      "ext": "https://www.chaojidianshi.net/api.php/app/live"
+    },
+    {
+      "name": "LiveTV123",
+      "api": "csp_LiveQ",
+      "ext": "https://livenew123.top/live.json"
+    }
+  ],
+  "sites": [
+    {
+      "key": "drpy_js_多多",
+      "name": "多多影視",
+      "type": 3,
+      "api": "https://ghproxy.com/https://raw.githubusercontent.com/hjdhnx/dr_py/main/js/ddys.js"
+    },
+    {
+      "key": "Git短劇",
+      "name": "短劇專區",
+      "type": 3,
+      "api": "https://ghproxy.com/https://raw.githubusercontent.com/FongMi/CatVodSpider/main/json/short.json"
+    },
+    {
+      "key": "Git影院",
+      "name": "Git影院",
+      "type": 1,
+      "api": "https://ghproxy.com/https://raw.githubusercontent.com/FongMi/CatVodSpider/main/json/m.json"
+    }
+  ]
+}
