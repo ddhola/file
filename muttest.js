20697c62c7c3341129316753d4e21ab2f052dc4d
ddhola <landwhite9@gmail.com>
2025-07-10 17:15:35 +0800

    Update muttest.js

--- muttest.js
+++ muttest.js
@@ -6,13 +6,26 @@
 {"name": "59 🇹🇼", "api": "csp_MQiTV", "ext": "https://59.125.210.231:4433"},
 {"name":"立視 4gtv🇹🇼","type": 0,"url": "http://xg.52sw.top/iptv/litv.php","ua": "okhttp/3.15","epg": "https://cdn.1678520.xyz/epg/?ch={name}&date={date}","logo":"https://epg.112114.xyz/logo/{name}.png"},    
 
+{"name":"01web.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/01web.txt"},	
 {"name":"0606.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/0606.txt"},	
 {"name":"0609test","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/0609test"},	
 {"name":"1027","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/1027"},	
-{"name":"xcollect.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/xcollect.txt"}
+{"name":"122","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/122.txt"},	
+{"name":"4g_breezy","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/4g_breezy.txt"},	
+{"name":"bull","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/bull.txt"},	
+{"name":"movie","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/movie.txt"},	
+{"name":"movie_action","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/movie_action.txt"},
+
+{"name":"x1pd","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/x1pd"},	
+{"name":"x_jable_c_17150.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/x_jable_c_17150.txt"},	
+{"name":"xblue.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/xblue.txt"},	
+{"name":"xcar","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/xcar"},	
+{"name":"xch2406.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/xch2406.txt"},	
+{"name":"xcollect.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/xcollect.txt"},	
+{"name":"xheyzo","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/xheyzo"}
 
 
-
+ 
 
 ]
 
