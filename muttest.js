bf025637d6e4729369a60fe31b939d3a7d7c56e7
ddhola <landwhite9@gmail.com>
2025-07-10 17:04:44 +0800

    Update muttest.js

--- muttest.js
+++ muttest.js
@@ -6,32 +6,10 @@
 {"name": "59 🇹🇼", "api": "csp_MQiTV", "ext": "https://59.125.210.231:4433"},
 {"name":"立視 4gtv🇹🇼","type": 0,"url": "http://xg.52sw.top/iptv/litv.php","ua": "okhttp/3.15","epg": "https://cdn.1678520.xyz/epg/?ch={name}&date={date}","logo":"https://epg.112114.xyz/logo/{name}.png"},    
 
-{"name":"0606.txt","https://raw.githubusercontent.com/ddhola/file/refs/heads/main/0606.txt"}, 
-{"name":"0609test.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/0609test.txt"}, 
-{"name":"1027.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/1027.txt"}, 
-{"name":"122.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/122.txt"}, 
-{"name":"4g_breezy.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/4g_breezy.txt"}, 
-{"name":"movie.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/movie.txt"},	
-{"name":"movie_action","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/movie_action.txt","ua": "okhttp/3.15","epg": "https://cdn.1678520.xyz/epg/?ch={name}&date={date}","logo":"https://epg.112114.xyz/logo/{name}.png"},  
-
-	  
-	  
-{"name": "AKTV", "type": 0, "url": "http://aktv.space/live.m3u", "ua": "okhttp/3.15", "epg": "https://iptv.crestekk.cn/epgphp/?ch={name}&date={date}"},
-{"name": "LITV 云梯", "type": 3, "api": "https://gh-proxy.com/raw.githubusercontent.com/alenin-zhang/IPTV/master/live_litv.py", "ext": {}},
-{"name": "LiTV 52sw", "type": 0, "url": "http://xg.52sw.top/iptv/litv.php", "ua": "okhttp/3.15", "epg": "http://iptv.52sw.top:668/?ch={name}&date={date}", "pass": true},
-{"name": "WEBTV 港台", "type": 0, "url": "https://gh-proxy.com/raw.githubusercontent.com/alenin-zhang/IPTV/master/web2.txt", "ua": "okhttp/3.15", "epg": "https://iptv.crestekk.cn/epgphp/?ch={name}&date={date}"},
-{"name":"Catvod smart","url":"https://live.catvod.com/smart.php"},
-
-
-{"name":"Judy","url":"https://raw.githubusercontent.com/sqspot/tac/refs/heads/main/tvbus/merged_judy.m3u"},
-{"name":"smt_cnlive.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/smt_cnlive.txt"},
-{"name":"x_jable_c_17150.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/x_jable_c_17150.txt"},	
-{"name":"xblue.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/xblue.txt"},	
-{"name":"xch2406.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/xch2406.txt"},	
-{"name":"x1pd","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/x1pd"},	
-{"name":"xcar","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/xcar"},	
-{"name":"xheyzo","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/xheyzo"},	
-{"name":"xcollect.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/xcollect.txt"},	
+{"name":"0606.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/0606.txt"},	
+{"name":"0609test","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/0609test"},	
+{"name":"1027","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/1027"},	
+{"name":"xcollect.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/xcollect.txt"}
 
 
 
