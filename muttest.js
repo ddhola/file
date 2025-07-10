efb2ef7239bfcf13fa9801539c8f95748e2e2ef7
ddhola <landwhite9@gmail.com>
2025-07-10 16:52:53 +0800

    Update muttest.js

--- muttest.js
+++ muttest.js
@@ -6,12 +6,15 @@
 {"name": "59 🇹🇼", "api": "csp_MQiTV", "ext": "https://59.125.210.231:4433"},
 {"name":"立視 4gtv🇹🇼","type": 0,"url": "http://xg.52sw.top/iptv/litv.php","ua": "okhttp/3.15","epg": "https://cdn.1678520.xyz/epg/?ch={name}&date={date}","logo":"https://epg.112114.xyz/logo/{name}.png"},    
 
-{"name":"test.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/test.txt"}, 
-{"name":"test2.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/test2.txt"}, 
-{"name":"test3.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/test3.txt"}, 
-{"name":"teststream.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/teststream.txt"}, 
-{"name":"0709web.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/0709web.txt"}, 
+{"name":"0606.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/0606.txt"}, 
+{"name":"0609test.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/0609test.txt"}, 
+{"name":"1027.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/1027.txt"}, 
 {"name":"122.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/122.txt"}, 
+{"name":"4g_breezy.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/4g_breezy.txt"}, 
+{"name":"movie.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/movie.txt"},	
+{"name":"movie_action","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/movie_action.txt","ua": "okhttp/3.15","epg": "https://cdn.1678520.xyz/epg/?ch={name}&date={date}","logo":"https://epg.112114.xyz/logo/{name}.png"},  
+
+	  
 	  
 {"name": "AKTV", "type": 0, "url": "http://aktv.space/live.m3u", "ua": "okhttp/3.15", "epg": "https://iptv.crestekk.cn/epgphp/?ch={name}&date={date}"},
 {"name": "LITV 云梯", "type": 3, "api": "https://gh-proxy.com/raw.githubusercontent.com/alenin-zhang/IPTV/master/live_litv.py", "ext": {}},
@@ -21,21 +24,7 @@
 
 
 {"name":"Judy","url":"https://raw.githubusercontent.com/sqspot/tac/refs/heads/main/tvbus/merged_judy.m3u"},
-
-  
-{"name":"0606.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/0606.txt"},	
-{"name":"0609test","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/0609test"},	
-{"name":"1027","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/1027"},	
-{"name":"0708.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/0708.txt"},	
-
-{"name":"4g_breezy.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/4g_breezy.txt"},	
-
-{"name":"movie.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/movie.txt"},	
-{"name":"movie_action","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/movie_action.txt","ua": "okhttp/3.15","epg": "https://cdn.1678520.xyz/epg/?ch={name}&date={date}","logo":"https://epg.112114.xyz/logo/{name}.png"},  
-
-    
 {"name":"smt_cnlive.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/smt_cnlive.txt"},
-  
 {"name":"x_jable_c_17150.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/x_jable_c_17150.txt"},	
 {"name":"xblue.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/xblue.txt"},	
 {"name":"xch2406.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/xch2406.txt"},	
@@ -44,17 +33,7 @@
 {"name":"xheyzo","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/xheyzo"},	
 {"name":"xcollect.txt","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/xcollect.txt"},	
 
-{"name":"TAC_live","url":"https://raw.githubusercontent.com/sqspot/tac/refs/heads/main/tvbus/live.txt"},
-{"name":"TAC_live2","url":"https://raw.githubusercontent.com/sqspot/tac/refs/heads/main/%E7%9B%B4%E6%92%AD%E6%BA%90/live2.txt"},
-{"name":"TAC_live3","url":"https://raw.githubusercontent.com/sqspot/tac/refs/heads/main/%E7%9B%B4%E6%92%AD%E6%BA%90/live3.txt"},
-{"name":"TAC_livex2","url":"https://raw.githubusercontent.com/sqspot/tac/refs/heads/main/%E7%9B%B4%E6%92%AD%E6%BA%90/livex2.txt"},
-{"name":"TAC_livex3","url":"https://raw.githubusercontent.com/sqspot/tac/refs/heads/main/%E7%9B%B4%E6%92%AD%E6%BA%90/livex3.txt"},
 
-{"name":"checked.m3u","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/checked.m3u"},
-{"name":"影視1","url":"https://raw.githubusercontent.com/sqspot/tac/refs/heads/main/%E7%9B%B4%E6%92%AD%E6%BA%90/%E5%BD%B1%E8%A6%96%E9%9B%861.txt","ua": "okhttp/3.15","epg": "https://cdn.1678520.xyz/epg/?ch={name}&date={date}","logo":"https://epg.112114.xyz/logo/{name}.png"},  
-{"name":"影視2","url":"https://raw.githubusercontent.com/sqspot/tac/refs/heads/main/%E7%9B%B4%E6%92%AD%E6%BA%90/%E5%BD%B1%E8%A6%96%E9%9B%862.txt","ua": "okhttp/3.15","epg": "https://cdn.1678520.xyz/epg/?ch={name}&date={date}","logo":"https://epg.112114.xyz/logo/{name}.png"}  
-
- 
 
 
 ]
