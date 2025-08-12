cab5b2b5cbc43b4d7ce8bf23abc0073365c9fdac
ddhola <landwhite9@gmail.com>
2025-08-12 20:44:06 +0800

    Update muttest.js

--- muttest.js
+++ muttest.js
@@ -6,6 +6,8 @@
 
 {"name": "590 🇹🇼", "api": "csp_MQiTV", "ext": "https://59.125.210.231:4433","epg": "https://cdn.1678520.xyz/epg/?ch={name}&date={date}","logo":"https://epg.112114.xyz/logo/{name}.png"},    
 {"name": "60 🇹🇼", "api": "csp_MQiTV", "ext": "https://60.248.127.232:4433"},
+{"name":"1027     ","url":"https://raw.githubusercontent.com/ddhola/file/refs/heads/main/1027"},	
+    
 {"name":"立視 4gtv🇹🇼","type": 0,"url": "http://xg.52sw.top/iptv/litv.php","ua": "okhttp/3.15","epg": "https://cdn.1678520.xyz/epg/?ch={name}&date={date}","logo":"https://epg.112114.xyz/logo/{name}.png"},    
 
 {"name":"sun","url":"https://igdux.top/BNMe"},    
